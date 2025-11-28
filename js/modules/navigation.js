c0a26fdc4490c958a01e05cb64735008e44c81e6
1329655015-ux <1329655015@qq.com>
2025-11-28 18:06:31 +0800

    Update navigation.js

--- js/modules/navigation.js
+++ js/modules/navigation.js
@@ -1,1 +1,21 @@
-// navigation
+// 导航模块
+const Navigation = {
+    init() {
+        console.log('导航模块已初始化');
+        // 这里将实现导航功能
+    }
+};
+
+window.Navigation = Navigation;
+js/modules/search.js
+
+javascript
+// 搜索模块
+const Search = {
+    init() {
+        console.log('搜索模块已初始化');
+        // 这里将实现搜索功能
+    }
+};
+
+window.Search = Search;
